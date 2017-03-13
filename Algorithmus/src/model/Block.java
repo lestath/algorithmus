f66d09dd81553eb1580c00a231eb64e1732af5d5
Mateusz Pelka <mat.pelka@gmail.com>
2017-03-13 16:02:01 +0100

    add: block interface

--- Algorithmus/src/model/Block.java
+++ Algorithmus/src/model/Block.java
@@ -4,6 +4,7 @@ import java.awt.*;
 public abstract class Block {
     Object previous = null;
     Object next = null;
+    Object handle;
     Point pos = null;
     Dimension size = null;
     String content = "";
